2c062b264cce1f143fd111c2b498ca68f41efdf5
claudiovb <claudiocarvalhovilasboas@gmail.com>
2026-01-22 21:17:42 -0300

    update version for iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "wdk-swift-core",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
         .macOS(.v12)
     ],
     products: [
